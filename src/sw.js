587e29177b999ea8edfb1a23f26641ad6f0618a3
Osusu Osusu <osusu4@gmail.com>
2023-06-24 02:11:56 +0100

    using service worker default

---
+++ src/sw.js
@@ -0,0 +1,33 @@
+let cacheData='appV1'; 
+
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                // `${process.env.PUBLIC_URL}/static/js/bundle.js`,
+                // `${process.env.PUBLIC_URL}/index.html`,
+                // `${process.env.PUBLIC_URL}/index.html/`,
+                // `${process.env.PUBLIC_URL}/index.html/home`,
+                // `${process.env.PUBLIC_URL}/index.html/signup`,
+                // `${process.env.PUBLIC_URL}/index.html/add`,
+                // `${process.env.PUBLIC_URL}/index.html/news`,
+
+
+                `${process.env.PUBLIC_URL}/static/js/bundle.js`,
+            ])
+        })
+    )
+})
+
+
+this.addEventListener("fetch", (event)=>{
+    if(!navigator.onLine){
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if(resp){
+                    return resp; 
+                }
+            })
+        )
+    }
+})
